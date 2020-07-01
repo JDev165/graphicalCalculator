21598c5339f6877ecfe2e57afbf35756cab070fe
julio lemus <juliolemus@Mark-50.local>
2020-06-30 22:13:05 -0700

    Adding more to stack test

---
+++ expression.py
@@ -0,0 +1,15 @@
+# Don't forget to add Python function annotations
+
+def Expression:
+    def __init__(self):
+        # DST = Data Structure
+        self.expressionDST = None
+
+    def save(self, data):
+        pass
+
+    def evaluate(self) -> str:
+        pass
+
+    def clear(self):
+        pass
--- stack_test.py
+++ stack_test.py
@@ -9,14 +9,22 @@ class TestLinkedListStack(unittest.TestCase):
 
     def test_stack_push(self):
         expected_operators = ['÷', 'x', '+', '-']
-        expression = '(2+2)*(2+2)'
+        expression = '(2+2)x(2+2)'
+        parsed_expression = ['']
         operators = Stack()
         operands = Stack()
 
-        for value in expression:
-            if value in expected_operators:
-                operators.push(value)
-            else if type(value) in [int, float]:
-                operands.push(value)
+        for token in expression:
+            last_token = parsed_expression[-1]
+            if (last_token.isdigit() or '.' in last_token or last_token == '') and (token not in operators):
+                parsed_expression[-1] += token
             else:
-                continue
+                parsed_expression.append(token)
+
+        parsed_expression = ' '.join(parsed_expression)
+
+        for token in parsed_expression:
+        	if token in operators:
+        		operands.push()
+        	else:
+        		operators.push()	       
